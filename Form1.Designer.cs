49ce35e2402612a7c3150e8152a8b6209866862e
AlanLo <alanlo789@yahoo.com.hk>
2019-08-21 18:04:17 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace AutoFilePermission
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.labMsg = new System.Windows.Forms.Label();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 199);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(173, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(192, 199);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(173, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // labMsg
+            // 
+            this.labMsg.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labMsg.Location = new System.Drawing.Point(12, 225);
+            this.labMsg.Name = "labMsg";
+            this.labMsg.Size = new System.Drawing.Size(353, 21);
+            this.labMsg.TabIndex = 3;
+            this.labMsg.Text = "XXX";
+            this.labMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(12, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(353, 22);
+            this.tbPath.TabIndex = 1;
+            this.tbPath.Text = global::AutoFilePermission.Properties.Settings.Default._path;
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(12, 40);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.ReadOnly = true;
+            this.rtbLog.Size = new System.Drawing.Size(353, 153);
+            this.rtbLog.TabIndex = 4;
+            this.rtbLog.Text = "";
+            this.rtbLog.TextChanged += new System.EventHandler(this.RtbLog_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 255);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.labMsg);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "AutoFilePermission";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label labMsg;
+        private System.Windows.Forms.RichTextBox rtbLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace AutoFilePermission
+{
+    public partial class Form1 : Form
+    {
+        FileSystemWatcher fsw;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximumSize = MinimumSize = this.Size;
+            MaximizeBox = false;
+            fsw = new FileSystemWatcher
+            {
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.DirectoryName,
+                IncludeSubdirectories = true,
+            };
+            FileSystemEventHandler fseh = new FileSystemEventHandler(FileSystemWatcherOnChanged);
+            fsw.Created += fseh;
+            fsw.Changed += fseh;
+            fsw.Renamed += new RenamedEventHandler(FileSystemWatcherOnRenamed);
+            fsw.Deleted += new FileSystemEventHandler(FileSystemWatcherOnDeleted);
+
+            labMsg.Text = "No service is running";
+            btnStop.Enabled = false;
+        }
+
+        private void FileSystemWatcherOnDeleted(object sender, FileSystemEventArgs e)
+        {
+            string path = e.FullPath;
+            this.Invoke((MethodInvoker)delegate
+            {
+                rtbLog.Text += DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + e.ChangeType + " " + path + "\n";
+            });
+        }
+
+        private void FileSystemWatcherOnRenamed(object sender, RenamedEventArgs e)
+        {
+            string path = e.FullPath;
+            try
+            {
+                //GrantAccess(path);
+            }
+            catch { }
+            this.Invoke((MethodInvoker)delegate
+            {
+                rtbLog.Text += DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + e.ChangeType + " " + path + "\n";
+            });
+        }
+
+        private void FileSystemWatcherOnChanged(object sender, FileSystemEventArgs e)
+        {
+            string path = e.FullPath;
+            try
+            {
+                //GrantAccess(path);
+            }
+            catch { }
+            this.Invoke((MethodInvoker)delegate
+            {
+                rtbLog.Text += DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + e.ChangeType + " " + path + "\n";
+            });
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            string path = tbPath.Text;
+            if (!String.IsNullOrWhiteSpace(path) && Directory.Exists(path))
+            {
+                fsw.Path = path;
+                fsw.EnableRaisingEvents = true;
+                btnStart.Enabled = false;
+                btnStop.Enabled = true;
+                Properties.Settings.Default._path = path;
+                Properties.Settings.Default.Save();
+                labMsg.Text = "Watching: " + path;
+            }
+            else
+            {
+                MessageBox.Show("Directory do not exist!");
+            }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            fsw.EnableRaisingEvents = false;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            labMsg.Text = "No service is running";
+        }
+
+        private void GrantAccess(string fullPath)
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(fullPath);
+            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+            dSecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+            dInfo.SetAccessControl(dSecurity);
+        }
+
+        private void RtbLog_TextChanged(object sender, EventArgs e)
+        {
+            // set the current caret position to the end
+            rtbLog.SelectionStart = rtbLog.Text.Length;
+            // scroll it automatically
+            rtbLog.ScrollToCaret();
+        }
+    }
+}
