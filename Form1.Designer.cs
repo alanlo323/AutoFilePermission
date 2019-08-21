d96cfca46c16b35d1b9b379401498e52ac0d3f6b
AlanLo <alanlo789@yahoo.com.hk>
2019-08-22 03:05:24 +0800

    new option for change permission

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,15 +31,16 @@
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.labMsg = new System.Windows.Forms.Label();
-            this.tbPath = new System.Windows.Forms.TextBox();
             this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.cbAutoPermission = new System.Windows.Forms.CheckBox();
+            this.tbPath = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(12, 199);
+            this.btnStart.Location = new System.Drawing.Point(12, 231);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(173, 23);
+            this.btnStart.Size = new System.Drawing.Size(249, 23);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -47,9 +48,9 @@
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(192, 199);
+            this.btnStop.Location = new System.Drawing.Point(267, 231);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(173, 23);
+            this.btnStop.Size = new System.Drawing.Size(249, 23);
             this.btnStop.TabIndex = 2;
             this.btnStop.Text = "Stop";
             this.btnStop.UseVisualStyleBackColor = true;
@@ -58,36 +59,50 @@
             // labMsg
             // 
             this.labMsg.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labMsg.Location = new System.Drawing.Point(12, 225);
+            this.labMsg.Location = new System.Drawing.Point(12, 257);
             this.labMsg.Name = "labMsg";
-            this.labMsg.Size = new System.Drawing.Size(353, 21);
+            this.labMsg.Size = new System.Drawing.Size(504, 21);
             this.labMsg.TabIndex = 3;
             this.labMsg.Text = "XXX";
             this.labMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // tbPath
-            // 
-            this.tbPath.Location = new System.Drawing.Point(12, 12);
-            this.tbPath.Name = "tbPath";
-            this.tbPath.Size = new System.Drawing.Size(353, 22);
-            this.tbPath.TabIndex = 1;
-            this.tbPath.Text = global::AutoFilePermission.Properties.Settings.Default._path;
             // 
             // rtbLog
             // 
             this.rtbLog.Location = new System.Drawing.Point(12, 40);
             this.rtbLog.Name = "rtbLog";
             this.rtbLog.ReadOnly = true;
-            this.rtbLog.Size = new System.Drawing.Size(353, 153);
+            this.rtbLog.Size = new System.Drawing.Size(504, 185);
             this.rtbLog.TabIndex = 4;
             this.rtbLog.Text = "";
             this.rtbLog.TextChanged += new System.EventHandler(this.RtbLog_TextChanged);
+            // 
+            // cbAutoPermission
+            // 
+            this.cbAutoPermission.Checked = global::AutoFilePermission.Properties.Settings.Default.auto_permission;
+            this.cbAutoPermission.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::AutoFilePermission.Properties.Settings.Default, "auto_permission", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.cbAutoPermission.Location = new System.Drawing.Point(385, 12);
+            this.cbAutoPermission.Name = "cbAutoPermission";
+            this.cbAutoPermission.Size = new System.Drawing.Size(131, 22);
+            this.cbAutoPermission.TabIndex = 5;
+            this.cbAutoPermission.Text = "Auto Give Full Access";
+            this.cbAutoPermission.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cbAutoPermission.UseVisualStyleBackColor = true;
+            this.cbAutoPermission.CheckedChanged += new System.EventHandler(this.CbAutoPermission_CheckedChanged);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(12, 12);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(367, 22);
+            this.tbPath.TabIndex = 1;
+            this.tbPath.Text = global::AutoFilePermission.Properties.Settings.Default._path;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(377, 255);
+            this.ClientSize = new System.Drawing.Size(528, 287);
+            this.Controls.Add(this.cbAutoPermission);
             this.Controls.Add(this.rtbLog);
             this.Controls.Add(this.labMsg);
             this.Controls.Add(this.btnStop);
@@ -108,6 +123,7 @@
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Label labMsg;
         private System.Windows.Forms.RichTextBox rtbLog;
+        private System.Windows.Forms.CheckBox cbAutoPermission;
     }
 }
 
